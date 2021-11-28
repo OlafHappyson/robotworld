7d341b94ac274e61cfd471cda18f36d35c92b8c3
OlafHappyson <95042777+OlafHappyson@users.noreply.github.com>
2021-11-28 03:06:19 +0100

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "db_password" {
+  description = "Value of the DB username for PSQL"
+  type        = string
+  default     = "12345"
+}
+variable "db_username" {
+  description = "Value of the DB username for PSQL"
+  type        = string
+  default     = "postgres"
+}
+
