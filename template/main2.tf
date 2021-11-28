be20832b2d5d91344c4d0774aadbd8cfbca40fba
OlafHappyson <95042777+OlafHappyson@users.noreply.github.com>
2021-11-28 11:43:35 +0100

    Add files via upload

---
+++ template/main2.tf
@@ -0,0 +1,25 @@
+
+resource "null_resource" "db_setup1" {
+
+  provisioner "local-exec" {
+
+    command = "psql -h localhost -p %PORT1% -U postgres -f ./sql/world.sql"
+    
+    environment = {
+      PGPASSWORD = "${var.db_password}"
+      PGUSER = "${var.db_username}"
+    }
+  }
+}
+resource "null_resource" "db_setup2" {
+
+  provisioner "local-exec" {
+
+    command = "psql -h localhost -p %PORT2%  -U postgres -f ./sql/world1.sql"
+    
+    environment = {
+      PGPASSWORD = "${var.db_password}"
+      PGUSER = "${var.db_username}"
+    }
+  }
+}
